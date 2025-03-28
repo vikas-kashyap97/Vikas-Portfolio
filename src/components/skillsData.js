221f9241d8fecb4c5ada3c1b350cf1c6eff72976
Vikas Kashyap <159269579+vikas-kashyap97@users.noreply.github.com>
2025-03-28 10:25:28 +0530

    commit

--- src/components/skillsData.js
+++ src/components/skillsData.js
@@ -1,52 +1,17 @@
 export const skillsArr = [
-  {
-    id: 1,
-    skillName: "HTML",
-    value: 95,
-  },
-  {
-    id: 2,
-    skillName: "CSS",
-    value: 80,
-  },
-  {
-    id: 3,
-    skillName: "JavaScript",
-    value: 80,
-  },
-  {
-    id: 4,
-    skillName: "Tailwind CSS",
-    value: 85,
-  },
-  {
-    id: 5,
-    skillName: "React JS",
-    value: 85,
-  },
-  {
-    id: 6,
-    skillName: "MongoDB",
-    value: 90,
-  },
-  {
-    id: 7,
-    skillName: "Express.js",
-    value: 80,
-  },
-  {
-    id: 8,
-    skillName: "Git",
-    value: 95,
-  },
-{
-    id: 9,
-    skillName: "GitHub",
-    value: 85,
-  },
-  {
-    id: 10,
-    skillName: "APIs",
-    value: 95,
-  },
+  { id: 1, skillName: "HTML", value: 95 },
+  { id: 2, skillName: "CSS", value: 80 },
+  { id: 3, skillName: "JavaScript", value: 80 },
+  { id: 4, skillName: "Tailwind CSS", value: 85 },
+  { id: 5, skillName: "React JS", value: 85 },
+  { id: 6, skillName: "MongoDB", value: 90 },
+  { id: 7, skillName: "Express.js", value: 80 },
+  { id: 8, skillName: "Git", value: 95 },
+  { id: 9, skillName: "GitHub", value: 85 },
+  { id: 10, skillName: "APIs", value: 95 },
+  { id: 11, skillName: "Node.js", value: 85 },
+  { id: 12, skillName: "Next.js", value: 80 },
+  { id: 13, skillName: "Developer Tools", value: 90 },
+  { id: 14, skillName: "Artificial Intelligence", value: 75 },
+  { id: 15, skillName: "GitHub Copilot", value: 80 },
 ];
